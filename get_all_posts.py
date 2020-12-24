a1da1ac2a563504b28bd706e6c3de603a91fa318
sh1n0b1n0m0n0 <nindzja.t001@gmail.com>
2020-12-24 21:40:39 +0300

    внёс в ф-цию возможность добавления имени группы

--- get_all_posts.py
+++ get_all_posts.py
@@ -2,5 +2,6 @@ from webapp import create_app
 from vk_parser import *
 
 app = create_app()
+group_name = 'warhammer_art_of_war'
 with app.app_context():
-    take_posts()
+    take_posts(group_name)
